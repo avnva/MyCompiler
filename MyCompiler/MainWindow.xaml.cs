9387febe2d6afddde8eff4300da70bd8a74e5351
avnva <voynova.03@inbox.ru>
2024-02-17 23:16:34 +0700

    Add avalone editor

--- MyCompiler/MainWindow.xaml.cs
+++ MyCompiler/MainWindow.xaml.cs
@@ -9,16 +9,42 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MyCompiler
+namespace MyCompiler;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+    private void textEditor_TextChanged(object sender, EventArgs e)
+    {
+        undoButton.IsEnabled = textEditor.CanUndo;
+        redoButton.IsEnabled = textEditor.CanRedo;
+
+        //undoMenuItem.IsEnabled = textEditor.CanUndo;
+        //redoMenuItem.IsEnabled = textEditor.CanRedo;
+
+        GetCaretPosition();
+    }
+
+    private void textEditor_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        GetCaretPosition();
+    }
+
+    private void textEditor_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        GetCaretPosition();
+    }
+
+    private void GetCaretPosition()
+    {
+        int offset = textEditor.CaretOffset;
+        var location = textEditor.Document.GetLocation(offset);
+        CursorPositionTextBlock.Text = $"Строка: {location.Line}, Столбец: {location.Column}";
     }
 }
